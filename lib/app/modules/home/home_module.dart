4cb3a76bcd60215ad741e6ff50618afa6c131b65
webpacs <webpacs.co@gmail.com>
2020-02-14 16:45:54 -0300

    Ajustes para receber instancias do Dio e NasaRepository do AppModule

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,23 +1,27 @@
-import 'package:nasa_picture_info/app/modules/home/detail/home_detail.dart';
-import 'package:nasa_picture_info/app/modules/home/detail/home_detail_controller.dart';
+import 'package:dio/dio.dart';
 import 'package:nasa_picture_info/app/modules/home/home_controller.dart';
-import 'package:nasa_picture_info/app/modules/home/repositories/home_repository.dart';
+import 'package:nasa_picture_info/app/modules/home/page/home_detail/home_detail.dart';
+import 'package:nasa_picture_info/app/modules/home/page/home_detail/home_detail_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nasa_picture_info/app/modules/home/home_page.dart';
+import 'package:nasa_picture_info/app/shared/repositories/nasa_repository.dart';
 
 import '../../app_module.dart';
 
 class HomeModule extends ChildModule {
+  final Dio dio = AppModule.to.get<Dio>();
+  final NasaRepository nasaRepository = AppModule.to.get<NasaRepository>();
+
   @override
   List<Bind> get binds => [
-        Bind((i) => HomeRepository(AppModule.to.get(), AppModule.to.get())),
-        Bind((i) => HomeController(AppModule.to.get(), i.get())),
+        Bind((i) => HomeController(dio, nasaRepository)),
         Bind((i) => HomeDetailController()),
       ];
 
   @override
   List<Router> get routers => [
         Router('/', child: (_, args) => HomePage()),
+        /*
         Router(
           '/details/:img/:date/:title/:ex',
           child: (_, args) => HomeDetail(
@@ -26,6 +30,12 @@ class HomeModule extends ChildModule {
             title: args.params['title'],
             explanation: args.params['ex'],
           ),
+          transition: TransitionType.fadeIn,
+        ),
+        */
+        Router(
+          '/detail',
+          child: (_, args) => HomeDetail(),
           transition: TransitionType.fadeIn,
         ),
       ];
