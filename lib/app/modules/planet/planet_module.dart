db444b56793de6577b768460280655a063f3b3e5
webpacs <webpacs.co@gmail.com>
2020-02-20 16:30:30 -0300

    Ajuste nas URLS do modulo

--- lib/app/modules/planet/planet_module.dart
+++ lib/app/modules/planet/planet_module.dart
@@ -1,3 +1,4 @@
+import 'package:nasa_picture_info/app/modules/planet/pages/planet_detail/planet_detail_page.dart';
 import 'package:nasa_picture_info/app/modules/planet/planet_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:nasa_picture_info/app/modules/planet/planet_page.dart';
@@ -10,7 +11,11 @@ class PlanetModule extends ChildModule {
 
   @override
   List<Router> get routers => [
-        Router('/detail', child: (_, args) => PlanetPage()),
+        Router('/', child: (_, args) => PlanetPage()),
+        Router('/detail/:planet',
+            child: (_, args) => PlanetDetailPage(
+                  args.params['planet'],
+                )),
       ];
 
   static Inject get to => Inject<PlanetModule>.of();
