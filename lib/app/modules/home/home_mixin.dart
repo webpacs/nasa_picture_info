b6b5543c0dec29746d6cbf22aeb276000ae96f7d
webpacs <webpacs.co@gmail.com>
2020-02-20 20:01:09 -0300

    Separacao de codigo no mixin

--- lib/app/modules/home/home_mixin.dart
+++ lib/app/modules/home/home_mixin.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:nasa_picture_info/app/modules/home/components/nasa_row.dart';
 import 'package:nasa_picture_info/app/modules/home/home_controller.dart';
+import 'package:nasa_picture_info/app/shared/models/nasa.dart';
+import 'package:nasa_picture_info/app/Theme.dart' as Theme;
 
 mixin HomeMixin {
   HomeController homeController;
 
-  Widget getAppBarWidget() {
-    return AppBar(
+  GradientAppBar getGradientAppBar() {
+    return GradientAppBar(
+      titleSpacing: 0.0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                'treva',
+                style: Theme.TextStyles.appBarTitle,
+              ),
+            ),
+          )
+        ],
+      ),
+      automaticallyImplyLeading: false,
       centerTitle: true,
-      backgroundColor: Colors.blue,
-      elevation: 0,
-      title: Text("NASA's Astronomy Picture of the Day"),
+      gradient: LinearGradient(
+        colors: [
+          Theme.Colors.appBarGradientStart,
+          Theme.Colors.appBarGradientEnd
+        ],
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            // do something
+          },
+        )
+      ],
+    );
+  }
+
+  Flexible getListView(List<Nasa> nasaList) {
+    return Flexible(
+      child: Container(
+        color: Theme.Colors.planetListBackground,
+        child: ListView.builder(
+          itemExtent: 160.0,
+          itemCount: nasaList.length,
+          itemBuilder: (_, index) => NasaRow(nasaList[index], index),
+        ),
+      ),
     );
   }
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,17 +1,11 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:nasa_picture_info/app/modules/home/home_controller.dart';
-import 'package:nasa_picture_info/app/modules/home/home_mixin.dart';
-import 'package:nasa_picture_info/app/modules/home/home_module.dart';
-import 'package:nasa_picture_info/app/modules/home/page/home_detail/home_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
+import 'package:nasa_picture_info/app/modules/home/home_controller.dart';
+import 'package:nasa_picture_info/app/modules/home/home_mixin.dart';
+import 'package:nasa_picture_info/app/modules/home/home_module.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -35,86 +29,20 @@ class _HomePageState extends State<HomePage> with HomeMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: getAppBarWidget(),
+      appBar: getGradientAppBar(),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Observer(
-            builder: (_) {
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Observer(builder: (_) {
               if (controller.nasaList == null) {
                 return Center(
                   child: CircularProgressIndicator(),
                 );
               }
-              return Expanded(
-                child: RefreshIndicator(
-                  onRefresh: controller.refresh,
-                  child: ListView.separated(
-                    itemCount: controller.nasaList.length,
-                    itemBuilder: (context, int index) {
-                      controller.imageVerify(index);
-                      var formatter = new DateFormat('MM/dd/yyyy');
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => HomeDetail(
-                                index: index,
-                                nasa: controller.nasaList[index],
-                              ),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          children: <Widget>[
-                            Hero(
-                              tag: 'image$index',
-                              child: controller.hasImage
-                                  ? CachedNetworkImage(
-                                      imageUrl:
-                                          controller.nasaList[index].hdurl,
-                                      placeholder: (context, url) =>
-                                          CircularProgressIndicator(),
-                                      errorWidget: (context, url, error) =>
-                                          Icon(Icons.error),
-                                      fit: BoxFit.cover,
-                                    )
-                                  : Container(
-                                      child: Text(
-                                        "Error to load image",
-                                        style: GoogleFonts.montserrat(
-                                          textStyle: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 21),
-                                        ),
-                                      ),
-                                    ),
-                            ),
-                            Text(
-                              formatter.format(controller.nasaList[index].date),
-                              style: GoogleFonts.montserrat(
-                                textStyle: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, int index) {
-                      return SizedBox(
-                        height: 30,
-                      );
-                    },
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+
+              return getListView(controller.nasaList);
+            }),
+          ]),
     );
   }
 }
