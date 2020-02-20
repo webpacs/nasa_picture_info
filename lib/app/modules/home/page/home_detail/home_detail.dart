444c6fd644963d95874036f8df975b6cbf2fa0ed
webpacs <webpacs.co@gmail.com>
2020-02-20 20:00:43 -0300

    Criacao do mixin para separacao de codigo

--- lib/app/modules/home/page/home_detail/home_detail.dart
+++ lib/app/modules/home/page/home_detail/home_detail.dart
@@ -1,117 +1,25 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 import 'package:nasa_picture_info/app/shared/models/nasa.dart';
 
-class HomeDetail extends StatefulWidget {
+import 'home_detail_mixin.dart';
+
+class HomeDetail extends StatelessWidget with HomeDetailMixin {
   final Nasa nasa;
-  final int index;
 
-  const HomeDetail({Key key, this.nasa, this.index}) : super(key: key);
-
-  @override
-  _HomeDetailState createState() => _HomeDetailState();
-}
-
-class _HomeDetailState extends State<HomeDetail> {
-  Nasa nasa;
-
-  @override
-  void initState() {
-    nasa = widget.nasa;
-    super.initState();
-  }
+  HomeDetail(this.nasa);
 
   @override
   Widget build(BuildContext context) {
-    var formatter = new DateFormat('MM/dd/yyyy');
-    String data = formatter.format(nasa.date);
-
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        color: Color(0xFF736AB7),
+        child: Stack(
           children: <Widget>[
-            Hero(
-              tag: 'image${widget.index}',
-              child: CachedNetworkImage(
-                imageUrl: nasa.hdurl,
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-                fit: BoxFit.cover,
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(top: 20, left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Details",
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(color: Colors.white, fontSize: 40),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10, left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Title: ${nasa.title}",
-                    softWrap: true,
-                    textAlign: TextAlign.start,
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10, left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Date: $data",
-                    softWrap: true,
-                    textAlign: TextAlign.start,
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 10, left: 10),
-                  alignment: Alignment.centerLeft,
-                  child: ExpandablePanel(
-                    theme: ExpandableThemeData(iconColor: Colors.white),
-                    header: Text(
-                      "Explanation",
-                      style: GoogleFonts.montserrat(
-                        textStyle: TextStyle(color: Colors.white, fontSize: 25),
-                      ),
-                    ),
-                    expanded: Text(
-                      nasa.explanation,
-                      softWrap: true,
-                      maxLines: 1,
-                      style: GoogleFonts.montserrat(
-                        textStyle: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                    ),
-                    collapsed: Text(
-                      nasa.explanation,
-                      softWrap: true,
-                      style: GoogleFonts.montserrat(
-                        textStyle: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
+            getBackground(nasa),
+            getGradient(),
+            getContent(nasa),
+            getToolbar(context),
           ],
         ),
       ),
---
+++ lib/app/modules/home/page/home_detail/home_detail_mixin.dart
@@ -0,0 +1,73 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:nasa_picture_info/app/modules/home/common/nasa_summary.dart';
+import 'package:nasa_picture_info/app/modules/home/common/separator.dart';
+import 'package:nasa_picture_info/app/shared/models/nasa.dart';
+import 'package:nasa_picture_info/app/shared/util/text_style.dart';
+
+mixin HomeDetailMixin {
+  Container getBackground(Nasa nasa) {
+    return Container(
+      child: CachedNetworkImage(
+        imageUrl: nasa.hdurl,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+        fit: BoxFit.cover,
+        height: 300.0,
+      ),
+      constraints: BoxConstraints.expand(height: 295.0),
+    );
+  }
+
+  Container getGradient() {
+    return Container(
+      margin: EdgeInsets.only(top: 190.0),
+      height: 110.0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[Color(0x00736AB7), Color(0xFF736AB7)],
+          stops: [0.0, 0.9],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(0.0, 1.0),
+        ),
+      ),
+    );
+  }
+
+  Container getContent(Nasa nasa) {
+    final _overviewTitle = "Overview".toUpperCase();
+    return Container(
+      child: ListView(
+        padding: EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+        children: <Widget>[
+          NasaSummary(
+            nasa,
+            0,
+            horizontal: false,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _overviewTitle,
+                  style: Style.headerTextStyle,
+                ),
+                Separator(),
+                Text(nasa.explanation, style: Style.commonTextStyle),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container getToolbar(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: BackButton(color: Colors.white),
+    );
+  }
+}
